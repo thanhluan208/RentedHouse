fbad79a30edbe42c0da1d55b2ea6d8ab0d0a49e2
thanhluan208 <45123959+thanhluan208@users.noreply.github.com>
2024-08-11 20:45:06 +0700

    resolve build error

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import AppThemeProvider from "./Providers/AppTheme.provider.tsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
+import 'pdfmake/build/vfs_fonts';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AppThemeProvider>
