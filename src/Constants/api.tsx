c51019665eb9a81d3a645edf8870f93196d05248
thanhluan208 <45123959+thanhluan208@users.noreply.github.com>
2024-08-17 19:39:33 +0700

    update base api

--- src/Constants/api.tsx
+++ src/Constants/api.tsx
@@ -1,4 +1,5 @@
-const base = "rentedhousebe-production.up.railway.app";
+const base = "https://rentedhousebe-production.up.railway.app/";
+// const base = "localhost:3000";
 
 //! House
 export const baseHouseApi = `${base}/house`;
