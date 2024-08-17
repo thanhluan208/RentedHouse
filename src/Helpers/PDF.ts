b5b4d8a53a57988dd9feaf204813f67890ce7b45
thanhluan208 <45123959+thanhluan208@users.noreply.github.com>
2024-08-17 21:54:25 +0700

    revert code

--- src/Helpers/PDF.ts
+++ src/Helpers/PDF.ts
@@ -1,21 +1,12 @@
 import * as pdfMake from "pdfmake/build/pdfmake";
 import { TDocumentDefinitions } from "pdfmake/interfaces";
-import * as pdfFonts from "pdfmake/build/vfs_fonts";
-import "pdfmake/build/vfs_fonts";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import 'pdfmake/build/vfs_fonts';
 
-console.log(pdfFonts.pdfMake?.vfs);
-(<any>pdfMake).vfs = null;
-const interval = setInterval(() => {
-  if (pdfFonts.pdfMake?.vfs) {
-    clearInterval(interval);
-    (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
-  } else {
-    console.log("Waiting for pdfMake.vfs to be available");
-  }
-}, 100);
+(<any>pdfMake).vfs = pdfFonts.pdfMake?.vfs;
 
 // Remove the assignment to the vfs property
 
-export const generatePDF: any = (dd: TDocumentDefinitions) => {
+export const generatePDF = (dd: TDocumentDefinitions) => {
   pdfMake.createPdf(dd).download();
 };
